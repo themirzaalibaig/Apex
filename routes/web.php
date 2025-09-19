6731b0608976c201f66c99b35cd5140658e79824
Ali Baig <baiga8424@gmail.com>
2025-09-19 22:51:23 +0500

    feat: add livewire components and routes for website sections
    Add new Livewire components and corresponding blade templates for blog, contact, services, portfolio, and testimonials sections. Register routes for these new components in web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Livewire\About;
 use App\Livewire\Home;
+use App\Livewire\Services;
+use App\Livewire\Portfolio;
+use App\Livewire\Testimonials;
+use App\Livewire\Blog;
+use App\Livewire\Contact;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +14,9 @@ Route::get('/', function () {
 });
 
 Route::get('/home', Home::class)->name('home');
+Route::get('/about', About::class)->name('about');
+Route::get('/services', Services::class)->name('services');
+Route::get('/portfolio', Portfolio::class)->name('portfolio');
+Route::get('/testimonials', Testimonials::class)->name('testimonials');
+Route::get('/blog', Blog::class)->name('blog');
+Route::get('/contact', Contact::class)->name('contact');
